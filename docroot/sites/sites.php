65d2d5adf734d97d083823f73540f96f585f98b8
vagrant <vagrant@local.cencosudshop.com>
2018-11-23 21:27:57 +0000

    CEN-0015: Adding sites and db code in sites/site/settings.php.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -5,6 +5,7 @@
  * Local development override configuration feature.
  */
 
-$sites['local.costaneracenter.com'] = 'costaneracenter';
 $sites['local.altolascondes.com'] = 'altolascondes';
 $sites['local.floridacenter.com'] = 'floridacenter';
+$sites['local.pnunoa.com'] = 'pnunoa';
+$sites['local.ccenter.com'] = 'ccenter';
