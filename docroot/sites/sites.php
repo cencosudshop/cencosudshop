0be7a86fe90ab914b5ca6ec8dae81129f1db27cf
Jorge Melo <jorgedjma@gmail.com>
2018-11-22 23:26:00 -0300

    CEN-0012: Adding costaneracenter new multisite to code.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -4,6 +4,6 @@
  * @file
  * Local development override configuration feature.
  */
-
+$sites['local.costaneracenter.com'] = 'costaneracenter';
 $sites['local.altolascondes.com'] = 'altolascondes';
 $sites['local.floridacenter.com'] = 'floridacenter';
