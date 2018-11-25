3cd9acf8b4c7d033fddb2bd0e6274eed23138f56
vagrant <vagrant@local.cencosudshop.com>
2018-11-25 17:22:25 +0000

    CEN-0017: Adding posorno multisite code to repository.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -9,3 +9,4 @@ $sites['local.altolascondes.com'] = 'altolascondes';
 $sites['local.floridacenter.com'] = 'floridacenter';
 $sites['local.pnunoa.com'] = 'pnunoa';
 $sites['local.ccenter.com'] = 'ccenter';
+$sites['local.posorno.com'] = 'posorno';
