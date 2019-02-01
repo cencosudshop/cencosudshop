70ffb4f6b910095b3fffec7e12b3fdc88754e6c4
Carlos Ospina <vagrant@local.cencosudshop.com>
2019-02-01 17:23:43 +0000

    CEN-0023: Adiciona la configuracion de exportacion en default y arregla los drush aliases con los dominios.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -800,6 +800,8 @@ if ($is_ah_env) {
       # $config['acquia_lift.settings']['credential']['site_id'] = 'cencosud_stage'; //Unique
       $config['acquia_contenthub.admin_settings']['client_name'] = 'cencosud_stage_4f9b31ff-53c0-4e14-bf48-34a862cf67bf';
       $config['acquia_contenthub.admin_settings']['origin'] = '16896391-50d0-4d5d-7025-c0eb27c08e14';
+      $config['acquia_contenthub.admin_settings']['webhook_uuid'] = '6ea318bb-071c-46c3-50ab-250a301cda8b';
+      $config['acquia_contenthub.admin_settings']['webhook_url'] = 'http://default.realmedianetwork.com/acquia-contenthub/webhook';
       break;
 
     case 'dev':
